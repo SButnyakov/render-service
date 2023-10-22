b45c49d4bebc62f4a6a64493d85db9862e570866
chukaevVS <vladchukaev22@mail.ru>
2023-10-22 23:11:46 +0300

    fix docker problems

--- dockerScript.sh
+++ dockerScript.sh
@@ -4,6 +4,6 @@ docker build -t backend-cont .
 cd ..
 cd ./frontend
 docker build -t front-cont .
-docker run --rm -d -p 3000:3000 -t front-cont
-docker run --rm -d --network host backend-cont
+docker run -d -p 3000:3000 -t front-cont:latest
+docker run -d -p 8080:8080 -t backend-cont:latest
 
