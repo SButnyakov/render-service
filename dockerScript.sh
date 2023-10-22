49f7715dbb3c33a196ba3806174e57f6671ea254
Bingelibangable <fil.bugrov@mail.ru>
2023-10-23 02:29:00 +0300

    deleted script and added discription for docker

--- dockerScript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ./backend-api
-docker build -t backend-cont .
-cd ..
-cd ./frontend
-docker build -t front-cont .
-docker run -d -p 3000:3000 -t front-cont:latest
-docker run -d -p 8080:8080 -t backend-cont:latest
-
