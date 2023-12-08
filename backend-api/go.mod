754a63816deef6d00ce76da5451729c7799a3e30
Sergey <sergey.butnyakov@gmail.com>
2023-12-08 13:14:42 +0300

    create /subscribe handler

--- backend-api/go.mod
+++ backend-api/go.mod
@@ -15,12 +15,15 @@ require (
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/jfeliu007/goplantuml v1.6.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/redis/go-redis/v9 v9.3.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
--- backend-api/internal/server/api/handlers/subscribe/subscribe.go
+++ backend-api/internal/server/api/handlers/subscribe/subscribe.go
@@ -1,12 +1,41 @@
 package subscribe
 
 import (
+	"backend-api/internal/config"
+	resp "backend-api/internal/lib/api/response"
+	"backend-api/internal/storage"
+	"errors"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"log/slog"
 	"net/http"
+	"time"
 )
 
-func New(log *slog.Logger) http.HandlerFunc {
+type SubscriptionProvider interface {
+	GetExpireDate(int64) (*time.Time, error)
+	Create(storage.Subscription) error
+	Update(subscription storage.Subscription) error
+}
+
+type SubscriptionTypeProvider interface {
+	GetID(string) (int64, error)
+}
+
+type PaymentCreator interface {
+	Create(storage.Payment) error
+}
+
+type PaymentTypeProvider interface {
+	GetID(string) (int64, error)
+}
+
+type Response struct {
+	resp.Response
+}
+
+func New(log *slog.Logger, cfg *config.Config, pCreator PaymentCreator, ptProvider PaymentTypeProvider,
+	stProvider SubscriptionTypeProvider, sProvider SubscriptionProvider) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fn := "handlers.subscribe.New"
 
@@ -15,5 +44,75 @@ func New(log *slog.Logger) http.HandlerFunc {
 			slog.String("request_id", middleware.GetReqID(r.Context())),
 		)
 
+		uid := r.Context().Value("uid").(int64)
+
+		pTypeId, err := ptProvider.GetID(cfg.Payments.SubPremiumMonth)
+		if err != nil {
+			log.Error("invalid payment type")
+			responseError(w, r, resp.Error("invalid payment type"), http.StatusBadRequest)
+			return
+		}
+
+		err = pCreator.Create(storage.Payment{
+			UserID:   uid,
+			TypeId:   pTypeId,
+			DateTime: time.Now()})
+		if err != nil {
+			log.Error("failed to create payment record")
+			responseError(w, r, resp.Error("failed to create payment record"), http.StatusInternalServerError)
+			return
+		}
+
+		sTypeId, err := stProvider.GetID(cfg.Subscriptions.Premium)
+		if err != nil {
+			log.Error("invalid subscription type")
+			responseError(w, r, resp.Error("invalid subscription type"), http.StatusBadRequest)
+			return
+		}
+
+		expireDate, err := sProvider.GetExpireDate(uid)
+		if err != nil {
+			if errors.Is(err, storage.ErrSubscriptionNotFound) {
+				err = sProvider.Create(storage.Subscription{
+					UserId:     uid,
+					TypeId:     sTypeId,
+					ExpireDate: time.Now().AddDate(0, 1, 0)})
+				if err != nil {
+					log.Error("failed to subscribe")
+					responseError(w, r, resp.Error("subscribing failed"), http.StatusInternalServerError)
+					return
+				}
+				responseOK(w, r)
+				return
+			} else {
+				log.Error("failed to get subscription info")
+				responseError(w, r, resp.Error("subscribing failed"), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		err = sProvider.Update(storage.Subscription{
+			UserId:     uid,
+			TypeId:     sTypeId,
+			ExpireDate: expireDate.AddDate(0, 1, 0)})
+		if err != nil {
+			log.Error("failed to subscribe")
+			responseError(w, r, resp.Error("subscribing failed"), http.StatusInternalServerError)
+			return
+		}
+
+		responseOK(w, r)
 	}
+}
+
+func responseError(w http.ResponseWriter, r *http.Request, response resp.Response, status int) {
+	w.WriteHeader(status)
+	render.JSON(w, r, response)
+}
+
+func responseOK(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	render.JSON(w, r, Response{
+		Response: resp.OK(),
+	})
 }
